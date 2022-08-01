bcf857c0ff30d2878f88502c6fbfc47455d83b9a
Dhaesha M <dhaesha1@gmail.com>
2022-08-01 15:38:36 -0400

    flex is not aligning my content how I want it so I will incorporate tailwind grids. Maybe 4 columns, 2 rows.

--- pages/404.js
+++ pages/404.js
@@ -1,15 +1,35 @@
 const UnknownPage = () => {
   return (
     // bg-slate-50/0
-    <div className="w-screen h-screen bg-[url('../public/grid.png')] bg-center bg-cover">
-      <div>
-        <div id="top-left" className="">
-          <h1 className="">Unknown</h1>
-        </div>
-        <div id="middle" className=""></div>
-        <div id="bottom-right">
-          <p className="">info</p>
-        </div>
+    <div className="w-screen h-screen bg-[url('../public/grid.png')] bg-center bg-cover flex flex-col">
+      {/* <div className=""> */}
+      <div id="top-left" className="">
+        <h1 className="">Defi Ours</h1>
+      </div>
+      {/* <div style="width:100%;height:0;padding-bottom:100%;position:relative;"> */}
+      <div
+        id="middle"
+        className="w-96 h-96 flex justify-self-center align-self-center"
+      >
+        <iframe
+          src="https://giphy.com/embed/gHi4YMtu5ilxykWCXj"
+          width="150%"
+          height="150%"
+          //   style="position:absolute"
+          frameBorder="0"
+          className="giphy-embed"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <p>
+        <a href="https://giphy.com/stickers/TRANS-EU-world-digital-lodz-gHi4YMtu5ilxykWCXj">
+          via GIPHY
+        </a>
+      </p>
+      <div id="bottom-right" className="flex flex-col">
+        <h1 className="">Page Not Found</h1>
+        <p>Click Here to Learn More About Defi Ours.</p>
+        <p>Click Here to Go Home.</p>
       </div>
     </div>
   );
