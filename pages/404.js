d5c125c5396135c1b80066f1ec588b54645b006d
Dhaesha M <dhaesha1@gmail.com>
2022-08-01 16:55:10 -0400

    components positioned using tailwind grid. will add images + make responsive

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,12 @@
 const UnknownPage = () => {
   return (
     <div className="w-screen h-screen bg-[url('../public/grid.png')] bg-center bg-cover grid grid-rows-2 grid-cols-4 gap-1">
-      <div id="top-left" className="col-span-1 row-span-1">
+      <div id="top-left" className="col-start-1 row-start-1">
         <h1 className="">Defi Ours</h1>
       </div>
       <div
         id="middle"
-        className="w-90 h-90 col-span-2 row-span-2 col-start-1 row-start-1"
+        className="w-90 h-90 col-span-2 row-span-2 col-start-1 row-start-1 flex flex-col "
       >
         <iframe
           src="https://giphy.com/embed/gHi4YMtu5ilxykWCXj"
@@ -18,14 +18,14 @@ const UnknownPage = () => {
           allowFullScreen
         ></iframe>
       </div>
-      <p>
+      <p className="col-start-3 row-start-3">
         <a href="https://giphy.com/stickers/TRANS-EU-world-digital-lodz-gHi4YMtu5ilxykWCXj">
           via GIPHY
         </a>
       </p>
       <div
         id="bottom-right"
-        className="col-span-1 row-span-1 col-start-3 row-start-3"
+        className="col-span-1 row-span-1 col-start-4 row-start-3"
       >
         <h1 className="">Page Not Found</h1>
         <p>Click Here to Learn More About Defi Ours.</p>
